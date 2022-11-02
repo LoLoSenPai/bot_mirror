3e0730ed13c18165c271f1d75897ec795920ed4c
LoLoSenPai <loic.dlugosz@gmail.com>
2022-11-02 15:49:28 +0100

    Update app.js
    added an other server to copy

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const client = new selfcore();
 const gateway = new selfcore.Gateway("MjgwNTE0MDk4NTk5NDI4MDk3.GQmMoh.8BI55MZytGHrBhWs1emJ1gYwYj22ErbPWsPsHQ");
 
 gateway.on("message", (m) => {
+  // Copy messages from Soultools server to Soloff server
   if (m.channel_id === "927748201107320852") {
     let content = m.content ? m.content : { embeds: [m.embeds[0]] };
     // let image = m.image ? m.image : m.attachments[0].url;
@@ -11,6 +12,15 @@ gateway.on("message", (m) => {
     client.sendWebhook(
       "https://discord.com/api/webhooks/1026842503623745637/jzgOolxRsSOL_5Vc23Q0C8-iBQ2QcIDuNU4gO5m91GOjWCVYvi8Q0Ri3_ClriBfKdJgw",
       content //+ "\n" + image
+    );
+  }
+  // Copy messages from Fractal server to Rabbit server
+  if (m.channel_id === "1037374542777176156") {
+    let content = m.content ? m.content : { embeds: [m.embeds[0]] };
+
+    client.sendWebhook(
+      "https://discord.com/api/webhooks/1037375634562891806/u0OgYZ4W_EEuBFbc91v1NB85E-on4LzGDUyhLsePFeqhdH6tf1-S6qWEq3u7xo_UZCJZ",
+      content
     );
   }
 });
