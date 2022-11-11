08c202ada131314ede5f9a034c19d0a4f6a3753c
LoLoSenPai <loic.dlugosz@gmail.com>
2022-11-11 21:37:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const gateway = new selfcore.Gateway("MjgwNTE0MDk4NTk5NDI4MDk3.GQmMoh.8BI55MZytG
 
 gateway.on("message", (m) => {
   // Copy messages from Soultools server to Soloff server
-  if (m.channel_id === "927748201107320852") {
-    let content = m.content ? m.content : { embeds: [m.embeds[0]] };
-    // let image = m.image ? m.image : m.attachments[0].url;
+  // if (m.channel_id === "927748201107320852") {
+  //   let content = m.content ? m.content : { embeds: [m.embeds[0]] };
+  //   // let image = m.image ? m.image : m.attachments[0].url;
 
-    client.sendWebhook(
-      "https://discord.com/api/webhooks/1026842503623745637/jzgOolxRsSOL_5Vc23Q0C8-iBQ2QcIDuNU4gO5m91GOjWCVYvi8Q0Ri3_ClriBfKdJgw",
-      content //+ "\n" + image
-    );
-  }
+  //   client.sendWebhook(
+  //     "https://discord.com/api/webhooks/1026842503623745637/jzgOolxRsSOL_5Vc23Q0C8-iBQ2QcIDuNU4gO5m91GOjWCVYvi8Q0Ri3_ClriBfKdJgw",
+  //     content //+ "\n" + image
+  //   );
+  // }
+
   // Copy messages from Fractal server to Rabbit server
   if (m.channel_id === "1037374542777176156") {
     let content = m.content ? m.content : { embeds: [m.embeds[0]] };
@@ -23,6 +24,7 @@ gateway.on("message", (m) => {
       content
     );
   }
+  
   // Copy messages from Gorilla server to Rabbit server
   if (m.channel_id === "993633368371638292") {
     let content = m.content ? m.content : { embeds: [m.embeds[0]] };
